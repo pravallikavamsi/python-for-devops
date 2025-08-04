f0342a10306c1d792a6564c31bc62debe406e71f
pravallikavamsi <114338840+pravallikavamsi@users.noreply.github.com>
2025-08-04 22:41:15 +0000

    file has been updated

--- Day-02/examples/01-string-split.py
+++ Day-02/examples/01-string-split.py
@@ -1,3 +1,10 @@
 text = "Python is awesome"
 words = text.split()
 print("Words:", words)
+word_count = len(words)
+print("number of words:", word_count)
+line = "python is a programming language"
+words = line.split()
+print('Words :', words)
+word_count = len(words)
+print('number of words:', word_count)
