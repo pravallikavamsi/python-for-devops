2ec49e1d7c3ddadd9eea73e3c75710cf4a294d93
pravallikavamsi <114338840+pravallikavamsi@users.noreply.github.com>
2025-08-04 22:30:20 +0000

    file hasbeen updated

--- Day-02/examples/01-string-replace.py
+++ Day-02/examples/01-string-replace.py
@@ -2,5 +2,5 @@ text = "Python is awesome"
 new_text = text.replace("awesome", "great")
 print("Modified text:", new_text)
 text = "my hobby is singing"
-new_text = text.replace("singing", "siging and dancing")
+new_text = text.replace("my hobby is singing", "her hobby is singing and dancing")
 print('modified text is', new_text)
