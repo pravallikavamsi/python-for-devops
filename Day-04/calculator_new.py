df3fb360a431aede1d74b77079f373d17348130f
pravallikavamsi <114338840+pravallikavamsi@users.noreply.github.com>
2025-08-10 16:06:17 +0000

    new file calculator_new.py hasbeen added

---
+++ Day-04/calculator_new.py
@@ -0,0 +1,15 @@
+a = 10
+b = 5
+def addition():
+    add = a + b
+    print("addition is", add)
+def subtraction():
+    sub = a - b
+    print("subtraction is", sub)
+def multiplication():
+    mul = a * b
+    print("multiplication is", mul)
+
+addition()
+subtraction()
+multiplication()
