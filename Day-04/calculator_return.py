bea9e2b30036fce5ce785a1fe9d7eef12b3de6ae
pravallikavamsi <114338840+pravallikavamsi@users.noreply.github.com>
2025-08-10 17:54:15 +0000

    new files have been added

---
+++ Day-04/calculator_return.py
@@ -0,0 +1,15 @@
+def addition(a, b):
+    add = a + b
+    return add
+def subtraction(a, b):
+    sub = a - b
+    return sub
+def multiplication(a, b):
+    mul = a * b
+    return mul
+
+print(addition(10, 5))
+print(subtraction(10, 5))
+print(multiplication(10,5))
+
+      
