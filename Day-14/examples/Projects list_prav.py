fb4f2a34ca2964fc4fde82c4bf231c7d8982946e
pravallikavamsi <114338840+pravallikavamsi@users.noreply.github.com>
2025-09-25 17:47:11 +0000

    new files added

---
+++ Day-14/examples/Projects list_prav.py
@@ -0,0 +1,21 @@
+# This code sample uses the 'requests' library:
+# http://docs.python-requests.org
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+url = "https://vallivamsi2018.atlassian.net/rest/api/3/project"
+API_TOKEN='ATATT3xFfGF0Ou-hAKdeWYw9Y0HYb9C6oX-ttN_d3j1nfbwYz0Sj5-47inHZvYb0nAW8pP5OfVpAqsmXW_dmJw_-WMwLJwPsC5G8JBSbyDLlQmJe07I4s4rtPzeAE-Fbp-iTnzgDpD2zcA5xxJu730CRAF1rn55W6Cj3dJmlJR1savdEJI_QSs8=508F929A' 
+auth = HTTPBasicAuth("vallivamsi2018@gmail.com", API_TOKEN)
+headers = {
+  "Accept": "application/json"
+}
+response = requests.request(
+   "GET",
+   url,
+   headers=headers,
+   auth=auth
+)
+output = json.loads(response.text)
+for project_list in [project_name["name"] for project_name in output]:
+   print(project_list)
+
